831ca605e34aabcce012ce82a0aa401126af9c71
Антон Неверович <dt@dilibrium.ru>
2020-05-29 11:44:25 +0300

    add create_headers() & update get_html() in utils.parsers

--- parsers/utils.py
+++ parsers/utils.py
@@ -1,11 +1,30 @@
 import requests
+import json
+import random
 
 
-def get_html(url: str) -> str:
+with open('../configs/user-agents.txt') as f:
+    USER_AGENTS = json.loads(f.read())
+
+
+def create_headers():
+    """Функция генерирует случайный HEADERS для обращения к серверу
+
+    :return: словарь, содержащий HEADERS
+    """
+
+    user_agent = random.choice(USER_AGENTS['browsers'][USER_AGENTS['randomize'][str(random.randint(0, 984))]])
+    return {"User-Agent": user_agent, "content-type": "text"}
+
+
+def get_html(url: str,
+             timeout=30) -> requests.models.Response:
     """Функция делает запрос по URL
 
     :param url: str -- URL-адрес
-    :return: HTML-страницу в виде текста
+    :param timeout: int -- задержка, по умолчанию 30 сек
+    :return: ответ сервера, объект requests.models.Response
     """
-    response = requests.get(url)
-    return response.text
+    headers = create_headers()
+    response = requests.get(url, timeout=timeout, headers=headers)
+    return response
